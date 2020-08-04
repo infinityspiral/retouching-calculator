fa238dbc480dc538643616946c870faa38a56d9d
Chris Norton <cnorton@aquent.com>
2020-08-04 11:14:30 -0500

    feat: number types, code cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,15 @@
 window.addEventListener('load', () => {
-  const perImageTotalMinutesEl = document.querySelector('#perImageTotalMinutes')
-  const perImageTotalHoursEl = document.querySelector('#perImageTotalHours')
-  const perImageTotalCostEl = document.querySelector('#perImageTotalCost')
-  const perImageTotalCostHalfEl = document.querySelector('#perImageTotalCostHalf')
+  const itemTimeControlsEl = document.querySelectorAll('.taskTimes input')
 
-  const quantityEl = document.querySelector('#quantity')
-  const perImageHourlyRateEl = document.querySelector('#perImageHourlyRate')
-  const revisionAllowanceEl = document.querySelector('#revisionAllowance')
-  const fileHandlingIntakeEl = document.querySelector('#fileHandlingIntake')
-  const rawProcessingEl = document.querySelector('#rawProcessing')
-  const clippingIsolationEl = document.querySelector('#clippingIsolation')
-  const cleanupEl = document.querySelector('#cleanup')
-  const colorCorrectionEl = document.querySelector('#colorCorrection')
-  const integrationEl = document.querySelector('#integration')
-  const moodGraceEl = document.querySelector('#moodGrace')
-  const fileOutputEl = document.querySelector('#fileOutput')
-  const deliveryEl = document.querySelector('#delivery')
-  const clearPerImageEl = document.querySelector('#clearPerImage')
-  clearPerImageEl.addEventListener('keyup', () => {
-    location.reload()
+  const allInputs = document.querySelectorAll('input')
+  allInputs.forEach(element => {
+    element.addEventListener('change',event => perImageBuildTotals())
   })
 
   const perImageRound1Minutes = () => {
-    return Number(fileHandlingIntakeEl.value) + Number(rawProcessingEl.value) + Number(clippingIsolationEl.value) + Number(cleanupEl.value) + Number(colorCorrectionEl.value) + Number(integrationEl.value) + Number(moodGraceEl.value) + Number(fileOutputEl.value) + Number(deliveryEl.value)
+    const itemTimeControlArr = Array.from(itemTimeControlsEl)
+    const itemTimeTotal = itemTimeControlArr.reduce((accum, item) => accum + Number(item.value), 0)
+    return itemTimeTotal
   }
 
   const perImageRound1MinutesWithRevisions = () => {
@@ -31,7 +18,7 @@ window.addEventListener('load', () => {
   }
 
   const perImageTotalMinutes = () => {
-    return perImageRound1MinutesWithRevisions() * Number(quantityEl.value)
+    return perImageRound1MinutesWithRevisions() * Number(document.querySelector('#quantity').value)
   }
 
   const perImageTotalHours = () => {
@@ -39,27 +26,19 @@ window.addEventListener('load', () => {
   }
 
   const perImageTotalCost = () => {
-    return perImageTotalHours() * Number(perImageHourlyRateEl.value)
+    return perImageTotalHours() * Number(document.querySelector('#perImageHourlyRate').value)
   }
+
+  const clearPerImageEl = document.querySelector('#clearPerImage')
+  clearPerImageEl.addEventListener('keyup', () => {
+    location.reload()
+  })
 
   const perImageBuildTotals = () => {
-    perImageTotalMinutesEl.innerHTML = parseInt(perImageTotalMinutes())
-    perImageTotalHoursEl.innerHTML = perImageTotalHours().toFixed(2)
-    perImageTotalCostEl.innerHTML = '$' + perImageTotalCost().toFixed(2)
-    perImageTotalCostHalfEl.innerHTML = '$' + (perImageTotalCost() / 2).toFixed(2)
+    document.querySelector('#perImageTotalMinutes').innerHTML = parseInt(perImageTotalMinutes())
+    document.querySelector('#perImageTotalHours').innerHTML = perImageTotalHours().toFixed(2)
+    document.querySelector('#perImageTotalCost').innerHTML = '$' + perImageTotalCost().toFixed(2)
+    document.querySelector('#perImageTotalCostHalf').innerHTML = '$' + (perImageTotalCost() / 2).toFixed(2)
   }
   perImageBuildTotals()
-
-  quantityEl.addEventListener('keyup', perImageBuildTotals)
-  perImageHourlyRateEl.addEventListener('keyup', perImageBuildTotals)
-  revisionAllowanceEl.addEventListener('keyup', perImageBuildTotals)
-  fileHandlingIntakeEl.addEventListener('keyup', perImageBuildTotals)
-  rawProcessingEl.addEventListener('keyup', perImageBuildTotals)
-  clippingIsolationEl.addEventListener('keyup', perImageBuildTotals)
-  cleanupEl.addEventListener('keyup', perImageBuildTotals)
-  colorCorrectionEl.addEventListener('keyup', perImageBuildTotals)
-  integrationEl.addEventListener('keyup', perImageBuildTotals)
-  moodGraceEl.addEventListener('keyup', perImageBuildTotals)
-  fileOutputEl.addEventListener('keyup', perImageBuildTotals)
-  deliveryEl.addEventListener('keyup', perImageBuildTotals)
 })
